e0b6abff78d68a8df847bfcdfd5760e677e4cad9
kraxel-dev <alexmeiyangzuo@gmail.com>
2025-02-18 17:08:18 +0100

    wrote alternative iteration callback that logs images in sorted order to rerun

--- include/fusion_helper/fusion_iteration_callback.h
+++ include/fusion_helper/fusion_iteration_callback.h
@@ -7,7 +7,6 @@
 #include <colmap/scene/image.h>
 #include <colmap/scene/point3d.h>
 #include <rerun.hpp>
-#include <stdio.h>
 
 namespace fuhe {
 
@@ -19,11 +18,6 @@ namespace fuhe {
  */
 class FusionIterationCallback : public ceres::IterationCallback {
  public:
-  const std::shared_ptr<rerun::RecordingStream> rr_rec;
-  const std::shared_ptr<rerun::Pinhole> rrpinhole;
-  const std::unordered_map<colmap::camera_t, colmap::Image>& images;
-  const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D;
-
   FusionIterationCallback(const std::shared_ptr<rerun::RecordingStream> rr_rec,
                           const std::shared_ptr<rerun::Pinhole> rrpinhole,
                           const std::unordered_map<colmap::camera_t, colmap::Image>& images,
@@ -36,17 +30,56 @@ class FusionIterationCallback : public ceres::IterationCallback {
     //  solver_options.logging_type = ceres::LoggingType::PER_MINIMIZER_ITERATION;
     // in the main executable
 
-    // -------------------- Update Rerun data
-    rr_rec->set_time_sequence("step", summary.iteration);
-
-    // colmap::PrintSolverSummary();
-    rrfuse::LogReconstruction(rr_rec, rrpinhole, images, points3D);
-
+    // -------------------- Log full reconstruction
+    this->UpdateIterationRerun(summary, this->images);
     return ceres::SOLVER_CONTINUE;
   }
 
- private:
-  // const bool log_to_stdout_ = false;
+ protected:
+  const std::shared_ptr<rerun::RecordingStream> rr_rec;
+  const std::shared_ptr<rerun::Pinhole> rrpinhole;
+  const std::unordered_map<colmap::camera_t, colmap::Image>& images;
+  const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D;
+
+  void UpdateIterationRerun(const ceres::IterationSummary& summary,
+                            const std::unordered_map<colmap::camera_t, colmap::Image>& target_images) {
+    // -------------------- Update Rerun data
+    rr_rec->set_time_sequence("step", summary.iteration);
+    rrfuse::LogReconstruction(rr_rec, rrpinhole, target_images, points3D);
+  }
+};
+
+/**
+ * @brief Ceres Callback, called during every iteration of ceres optimization. Derived to log colmap reconstruction to rerun.
+ Logs colmap images in sorted order.
+ * @ref 1. https://github.com/rerun-io/glomap/blob/main/glomap/estimators/global_positioning.cc#L26
+ *      2. http://ceres-solver.org/nnls_solving.html#_CPPv4N5ceres17IterationCallbackE
+ *
+ */
+class FusionIterationCallbackSorted : public FusionIterationCallback {
+ public:
+  FusionIterationCallbackSorted(const std::shared_ptr<rerun::RecordingStream> rr_rec,
+                                const std::shared_ptr<rerun::Pinhole> rrpinhole,
+                                const std::unordered_map<colmap::camera_t, colmap::Image>& images,
+                                const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D,
+                                const fuhe::types::MapOfImageIdsSec& img_ids_by_stamp)
+      : FusionIterationCallback(rr_rec, rrpinhole, images, points3D), img_ids_by_stamp{img_ids_by_stamp} {}
+
+  ceres::CallbackReturnType operator()(const ceres::IterationSummary& summary) {
+    std::unordered_map<colmap::camera_t, colmap::Image> imgs_sorted_by_stamp;
+    // iterate over all stamp sorted ids to have 'images' in sequential order
+    // for (auto& [_, id] : this->img_ids_by_stamp) {
+    //   imgs_sorted_by_stamp[id] = this->images.at(id);
+    // }
+
+    // this->UpdateIterationRerun(summary, imgs_sorted_by_stamp);
+    rr_rec->set_time_sequence("step", summary.iteration);
+    rrfuse::LogReconstructionSorted(rr_rec, rrpinhole, images, points3D, img_ids_by_stamp);
+    return ceres::SOLVER_CONTINUE;
+  }
+
+ protected:
+  fuhe::types::MapOfImageIdsSec img_ids_by_stamp;
 };
 
 }  // namespace fuhe
