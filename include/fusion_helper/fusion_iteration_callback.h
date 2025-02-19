e5d8ce6cf8cfd86840807434ea64e2e287335e7a
kraxel-dev <alexmeiyangzuo@gmail.com>
2025-02-19 15:10:08 +0100

    updated ceres rerun iteration callback - one class for vanilla colmap - new class for fusion rerun loggin

--- include/fusion_helper/fusion_iteration_callback.h
+++ include/fusion_helper/fusion_iteration_callback.h
@@ -11,17 +11,18 @@
 namespace fuhe {
 
 /**
- * @brief Ceres Callback, called during every iteration of ceres optimization. Derived to log colmap reconstruction to rerun.
+ * @brief Ceres iteration callback, called during every iteration of ceres optimization. Derived to log colmap reconstruction to rerun.
+ This is works with a vanilla colmap model.
  * @ref 1. https://github.com/rerun-io/glomap/blob/main/glomap/estimators/global_positioning.cc#L26
  *      2. http://ceres-solver.org/nnls_solving.html#_CPPv4N5ceres17IterationCallbackE
  *
  */
-class FusionIterationCallback : public ceres::IterationCallback {
+class BundleAdjustmentIterationCallback : public ceres::IterationCallback {
  public:
-  FusionIterationCallback(const std::shared_ptr<rerun::RecordingStream> rr_rec,
-                          const std::shared_ptr<rerun::Pinhole> rrpinhole,
-                          const std::unordered_map<colmap::camera_t, colmap::Image>& images,
-                          const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D)
+  BundleAdjustmentIterationCallback(const std::shared_ptr<rerun::RecordingStream> rr_rec,
+                                    const std::shared_ptr<rerun::Pinhole> rrpinhole,
+                                    const std::unordered_map<colmap::camera_t, colmap::Image>& images,
+                                    const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D)
       : rr_rec{rr_rec}, rrpinhole{rrpinhole}, images{images}, points3D{points3D} {}
 
   ceres::CallbackReturnType operator()(const ceres::IterationSummary& summary) {
@@ -50,20 +51,23 @@ class FusionIterationCallback : public ceres::IterationCallback {
 };
 
 /**
- * @brief Ceres Callback, called during every iteration of ceres optimization. Derived to log colmap reconstruction to rerun.
- Logs colmap images in sorted order.
+ * @brief Ceres iteration callback, called during every iteration of ceres optimization. Derived to log colmap fusion factor graph
+ to rerun. Logs colmap images in sorted order.
  * @ref 1. https://github.com/rerun-io/glomap/blob/main/glomap/estimators/global_positioning.cc#L26
  *      2. http://ceres-solver.org/nnls_solving.html#_CPPv4N5ceres17IterationCallbackE
  *
  */
-class FusionIterationCallbackSorted : public FusionIterationCallback {
+class FusionIterationCallback : public BundleAdjustmentIterationCallback {
  public:
-  FusionIterationCallbackSorted(const std::shared_ptr<rerun::RecordingStream> rr_rec,
-                                const std::shared_ptr<rerun::Pinhole> rrpinhole,
-                                const std::unordered_map<colmap::camera_t, colmap::Image>& images,
-                                const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D,
-                                const fuhe::types::MapOfImageIdsSec& img_ids_by_stamp)
-      : FusionIterationCallback(rr_rec, rrpinhole, images, points3D), img_ids_by_stamp{img_ids_by_stamp} {}
+  FusionIterationCallback(const std::shared_ptr<rerun::RecordingStream> rr_rec,
+                          const std::shared_ptr<rerun::Pinhole> rrpinhole,
+                          const std::unordered_map<colmap::camera_t, colmap::Image>& images,
+                          const std::unordered_map<colmap::point3D_t, colmap::Point3D>& points3D,
+                          const fuhe::types::MapOfImageIdsSec& img_ids_by_stamp,
+                          const std::map<const double, fuhe::OdomImagesEdge>& odom_edges)
+      : BundleAdjustmentIterationCallback(rr_rec, rrpinhole, images, points3D),
+        img_ids_by_stamp{img_ids_by_stamp},
+        odom_edges{odom_edges} {}
 
   ceres::CallbackReturnType operator()(const ceres::IterationSummary& summary) {
     std::unordered_map<colmap::camera_t, colmap::Image> imgs_sorted_by_stamp;
@@ -74,12 +78,14 @@ class FusionIterationCallbackSorted : public FusionIterationCallback {
 
     // this->UpdateIterationRerun(summary, imgs_sorted_by_stamp);
     rr_rec->set_time_sequence("step", summary.iteration);
-    rrfuse::LogReconstructionSorted(rr_rec, rrpinhole, images, points3D, img_ids_by_stamp);
+    rrfuse::LogReconstructionSorted(this->rr_rec, this->rrpinhole, this->images, this->points3D, this->img_ids_by_stamp);
+    rrfuse::LogOdometryEdges(this->rr_rec, this->images, this->odom_edges);
     return ceres::SOLVER_CONTINUE;
   }
 
  protected:
-  fuhe::types::MapOfImageIdsSec img_ids_by_stamp;
+  const fuhe::types::MapOfImageIdsSec img_ids_by_stamp;
+  const std::map<const double, fuhe::OdomImagesEdge> odom_edges;
 };
 
 }  // namespace fuhe
