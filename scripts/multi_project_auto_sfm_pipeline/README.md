f8245e8cc51f7e2b71506f7bbc7a7236c2768f87
kraxel-dev <alexmeiyangzuo@gmail.com>
2025-06-01 15:25:17 +0200

    first working draft of auto reconstruction pipeline of multi projects

--- scripts/multi_project_auto_sfm_pipeline/README.md
+++ scripts/multi_project_auto_sfm_pipeline/README.md
@@ -1,1 +1,27 @@
 Auto reconstruction pipeline to run colmap reconstruction on multiple projects at once (with shared param set)
+
+# Workpsace structure
+How each workspace/project you want to automatically reconstruct should be structured
+```
+.
+├── images
+│   ├── 1620666969669673472.png
+│   ├── 1620666969702673664.png
+│   ├── ...
+├── mask  # optional 
+│   ├── img_mask.png
+```
+After reconstruction the folder should look like this:
+```
+.
+├── db.db
+├── 0
+│   ├── images.bin
+│   ├── ...
+├── images
+│   ├── 1620666969669673472.png
+│   ├── 1620666969702673664.png
+│   ├── ...
+├── mask  # optional 
+│   ├── img_mask.png # if mask desired, name of mask-img must match exactly to this
+```
--- scripts/multi_project_auto_sfm_pipeline/reconstruct_multiple_projects.py
+++ scripts/multi_project_auto_sfm_pipeline/reconstruct_multiple_projects.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 def get_git_root() -> Path:
+    """Get the root directory of the Git repository this python script is lying in."""
     try:
         root = subprocess.check_output(["git", "rev-parse", "--show-toplevel"], text=True).strip()
         return Path(root)
@@ -11,7 +12,7 @@ def get_git_root() -> Path:
 
 
 def get_curr_pyscript_dir() -> Path:
-    # get the directory of the current script no matter where it is called from
+    """get the directory of the current script no matter where it is called from"""
     return Path(__file__).resolve().parent
 
 
@@ -30,12 +31,15 @@ class CmdArg:
 class ColmapCmdArgs:
     def __init__(self, ws_path, quality="low"):
         self.workspace_path = CmdArg("--workspace_path", ws_path)
+        self.output_path = CmdArg(
+            "--output_path", ws_path
+        )  # model output path is same as workspace/project path
         # subfolder to project ws path
         self.image_path = CmdArg("--image_path", ws_path / "images")
         self.database_path = CmdArg("--database_path", ws_path / f"db_{quality}_q.db")
         self.quality = CmdArg("--quality", quality)
 
-        # --- camera params
+        # --- camera and feature extratcion params
         self.single_camera = CmdArg("--ImageReader.single_camera", True)
         self.camera_model = CmdArg("--ImageReader.camera_model", "PINHOLE")
         # fx, fy, cx, cy
@@ -43,80 +47,17 @@ class ColmapCmdArgs:
             "--ImageReader.camera_params",
             "501.4757919305817, 501.4757919305817, 421.7953735163109, 167.65799492501083",
         )
+        self.camera_mask_path = CmdArg("--ImageReader.camera_mask_path", ws_path / "mask/img_mask.png")
 
         # --- sequential matcher params
         self.matcher_use_gpu = CmdArg("--SiftMatching.use_gpu", 0)
         self.matcher_quadratic_overlap = CmdArg("--SequentialMatching.quadratic_overlap", 1)
 
-
-def reconstruct_single_workspace(ws_path, model_quality="low"):
-    """reconstruct a single colmap model for a given a workspace. Workspace must contain populated /images subfolder.
-    quality settings {low, medium, high, extreme}
-    """
-
-    # --- check if images subfolder exists in project dir
-    imgs_sub_dir = ws_path / "images"
-    if not imgs_sub_dir.is_dir():
-        print(
-            f"Error: Subfolder '{imgs_sub_dir}' not found in '{ws_path}'. Make sure you have placed the /images folder into your project dir accordingly."
-        )
-        sys.exit(1)
-    else:
-        print(f"Images found for : {ws_path}")
-
-    # --- prepare command line arguments for colmap reconstruction
-    col_params = {}
-    # paths stuff
-    workspace_path = "--workspace_path"
-    image_path = "--image_path"
-    database_path = "--database_path"
-    quality = "--quality"
-    col_params[workspace_path] = ws_path
-    col_params[image_path] = imgs_sub_dir  # subfolder to project ws path
-    col_params[database_path] = ws_path / f"db_{model_quality}_q.db"
-    col_params[quality] = model_quality
-
-    # cam params
-    single_camera = "--ImageReader.single_camera"
-    camera_model = "--ImageReader.camera_model"
-    camera_params = "--ImageReader.camera_params"
-    col_params[single_camera] = 1
-    col_params[camera_model] = "PINHOLE"
-    col_params[camera_params] = (
-        "501.4757919305817, 501.4757919305817, 421.7953735163109, 167.65799492501083"  # fx, fy, cx, cy
-    )
-    exe = str(get_generic_colmap_exe_path() / "./colmap")
-
-    # --- Build the feature extraction command
-    extractor = "feature_extractor"
-    # fmt: off
-    command = [
-        exe, extractor, 
-        database_path, str(col_params[database_path]), 
-        image_path, str(col_params[image_path]), 
-        single_camera, str(col_params[single_camera]), 
-        camera_model, str(col_params[camera_model]), 
-        camera_params, str(col_params[camera_params]),
-    ]
-    # fmt: on
-    # --- Run the feature extractor command
-    print(f"Running command: {' '.join(command)}\nThis may take a while...\n")
-    try:
-        process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
-        )
-        # Print output line by line in real time
-        if process.stdout:
-            for line in process.stdout:
-                print(line, end="")  # avoid adding extra newlines
-        # Wait for the process to end
-        return_code = process.wait()
-        if return_code != 0:
-            print(f"\n Command failed with return code {return_code}.")
-    finally:
-        print(f"Failed to run feature extractor for {ws_path}.")
-        if process.stdout:
-            process.stdout.close()
+        # --- Mapper params
+        self.mapper_ba_refine_focal_length = CmdArg("--Mapper.ba_refine_focal_length", 0)
+        self.mapper_ba_refine_principal_point = CmdArg("--Mapper.ba_refine_principal_point", 0)
+        self.mapper_ba_refine_extra_params = CmdArg("--Mapper.ba_refine_extra_params", 0)
+        self.mapper_ba_use_gpu = CmdArg("--Mapper.ba_use_gpu", 0)
 
 
 def extract_features(cmd_args: ColmapCmdArgs):
@@ -125,9 +66,9 @@ def extract_features(cmd_args: ColmapCmdArgs):
         print(
             f"Error: Subfolder '{cmd_args.image_path.value}' not found in '{cmd_args.workspace_path.value}'. Make sure you have placed the /images folder into your project dir accordingly."
         )
-        sys.exit(1)
-    else:
-        print(f"Images found for : {cmd_args.workspace_path.value}")
+        return
+    
+    print(f"Images found for : {cmd_args.workspace_path.value}")
 
     # generic path to colmap exe given the root dir of this repo
     col_exe = str(get_generic_colmap_exe_path())
@@ -144,11 +85,25 @@ def extract_features(cmd_args: ColmapCmdArgs):
         cmd_args.camera_params.flag, str(cmd_args.camera_params.value),
     ]
     # fmt: on
+
+    # --- Check for mask path and add it to the command if it exists
+    if cmd_args.camera_mask_path.value.is_file():
+        print(f"Camera mask img detected: {cmd_args.camera_mask_path.value}")
+        command.append(cmd_args.camera_mask_path.flag)
+        command.append(str(cmd_args.camera_mask_path.value))
+
     # --- Run the feature extractor command
-    run_cmd(command)
+    run_cmd(command, extractor)
 
 
 def sequentially_match_imgs(cmd_args: ColmapCmdArgs):
+    if not cmd_args.database_path.value.is_file():
+        print(
+            f"Error: Database '{cmd_args.database_path.value}' does not exist. Skipping sequential matching!"
+        )
+        return
+
+    print(f"Database found for : {cmd_args.workspace_path.value}")
     col_exe = str(get_generic_colmap_exe_path())
     matcher = "sequential_matcher"
 
@@ -160,10 +115,47 @@ def sequentially_match_imgs(cmd_args: ColmapCmdArgs):
         cmd_args.database_path.flag, str(cmd_args.database_path.value),
     ]
     # fmt: on
-    run_cmd(command)
+    run_cmd(command, matcher)
 
 
-def run_cmd(command):
+def reoncstruct_model(cmd_args: ColmapCmdArgs):
+    if not cmd_args.database_path.value.is_file():
+        print(
+            f"Error: Database '{cmd_args.database_path.value}' does not exist. Skipping model reconstruction!"
+        )
+        return
+    elif not cmd_args.image_path.value.is_dir():
+        print(
+            f"Error: Images folder '{cmd_args.image_path.value}' does not exist. Skipping model reconstruction!"
+        )
+        return
+    
+    # --- cancel reoncstr if model already exists
+    if (cmd_args.output_path.value / "0").is_dir() and (cmd_args.output_path.value / "0" / "images.bin").is_file(): 
+        # if images.bin exists and is not empty, model already exists
+        if os.path.getsize(cmd_args.output_path.value / "0" / "images.bin") > 1000: # bytes
+            print(f"Model already exists at {cmd_args.output_path.value}. Skipping reconstruction.")
+            return
+
+    col_exe = str(get_generic_colmap_exe_path())
+    mapper = "mapper"
+
+    # fmt: off
+    command = [
+        col_exe, mapper,
+        cmd_args.database_path.flag, str(cmd_args.database_path.value),
+        cmd_args.image_path.flag, str(cmd_args.image_path.value),
+        cmd_args.output_path.flag, str(cmd_args.output_path.value),
+        cmd_args.mapper_ba_refine_focal_length.flag, str(cmd_args.mapper_ba_refine_focal_length.value),
+        cmd_args.mapper_ba_refine_principal_point.flag, str(cmd_args.mapper_ba_refine_principal_point.value),
+        cmd_args.mapper_ba_refine_extra_params.flag, str(cmd_args.mapper_ba_refine_extra_params.value),
+        cmd_args.mapper_ba_use_gpu.flag, str(cmd_args.mapper_ba_use_gpu.value),
+    ]
+    # fmt: on
+    run_cmd(command, mapper)
+
+
+def run_cmd(command, type):
     print(f"Running command: {' '.join(command)}\nThis may take a while...\n")
     try:
         process = subprocess.Popen(
@@ -178,14 +170,15 @@ def run_cmd(command):
         if return_code != 0:
             print(f"\n Command failed with return code {return_code}.")
     finally:
-        print(f"Failed to run feature extractor for {ws_path}.")
+        print(f"Failed to run {type} for {ws_path}.")
         if process.stdout:
             process.stdout.close()
 
 
 if __name__ == "__main__":
+
     # --- load yaml configuration file
-    cfg_path = get_curr_pyscript_dir() / "multi_config.yaml"
+    cfg_path = get_curr_pyscript_dir() / "multi_project_config.yaml"
     print(f"Loading configuration from {cfg_path}")
     with open(str(cfg_path), "r") as file:
         cfg = yaml.safe_load(file)
@@ -205,10 +198,11 @@ if __name__ == "__main__":
         # --- prepare command line arguments for colmap reconstruction
         col_args = ColmapCmdArgs(ws_path, reconstruction_quality)
 
-        # reconstruct_single_workspace(ws_path, reconstruction_quality)
         # --- perform feature extraction on current workspace
         extract_features(col_args)
 
         # --- sequentially match images on curr ws
         sequentially_match_imgs(col_args)
-        
+
+        # --- reconstruct model for current workspace
+        reoncstruct_model(col_args)
